043fdff39f895488e77c69a7cdbdf9b2f6a39d62
Harshit Batra <payafterwork.in@gmail.com>
2020-08-07 07:39:30 +0530

    User controller for more structured code

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/',(req, res)=>{
-	res.send("Hello yes");
-});
+const {sayHi} = require('../controllers/user');
+
+router.get('/',sayHi);
 
 
 module.exports = router;
