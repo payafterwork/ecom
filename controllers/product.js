6ae8d62c69aa2fec30b172bb78f74455a72406e6
Harshit Batra <payafterwork.in@gmail.com>
2020-08-10 16:57:30 +0530

    Product Validation - Fields & Photo

--- controllers/product.js
+++ controllers/product.js
@@ -8,18 +8,33 @@ const { errorHandler } = require('../helpers/dbErrorHandler');
 exports.create = (req, res) => {
     let form = new formidable.IncomingForm();
     form.keepExtensions = true;
-    form.parse(req, (err, fields, files) => {
+    form.parse(req, (err, fields, files) => { //fields has all the form data
 
         if (err) {
             return res.status(400).json({
                 error: 'Image could not be uploaded'
             });
         }
+
+        const { name, description, price, category, quantity, shipping } = fields;
+
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: 'All fields are required'
+            });
+        }
     
-        let product = new Product(fields);
+        let product = new Product(fields); //fields has all the form data
 
         if (files.photo) { //if files coming from form has phoyo
             //then in Product Model for photo field we fill up data & contentType fields
+
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1mb in size'
+                });
+            }
+
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type;
         }
