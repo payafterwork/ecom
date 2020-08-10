dc7e0be365c9771dcc5f119c1216e4a57f526e0e
Harshit Batra <payafterwork.in@gmail.com>
2020-08-10 18:43:12 +0530

    Product update - like create but extend (lodash)

--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,6 @@
 //To handle image upload we use formidable package
 const formidable = require('formidable');
-const _ = require('lodash'); // for certain functions we may use
+const _ = require('lodash'); // for ._extend used in update method
 const fs = require('fs'); //File system
 const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandler');
@@ -80,6 +80,44 @@ exports.remove = (req, res) => {
         }
         res.json({
             message: 'Product deleted successfully'
+        });
+    });
+};
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            });
+        }
+
+        let product = req.product; // rather than creating, request that product object
+        product = _.extend(product, fields); //we extend the fields with updated ones. 
+
+        // 1kb = 1000
+        // 1mb = 1000000
+
+        if (files.photo) {
+            // console.log("FILES PHOTO: ", files.photo);
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1mb in size'
+                });
+            }
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+        }
+
+        product.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(result);
         });
     });
 };
--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read, remove} = require('../controllers/product');
+const { create, productById, read, remove,update} = require('../controllers/product');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
@@ -15,7 +15,13 @@ router.delete("/product/:productId/:userId",
     remove
 );
 
-
+router.put( // put method is used for update
+    "/product/:productId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    update
+);
 
 router.param('userId', userById);
 router.param("productId", productById);
