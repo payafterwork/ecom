b9347fe975b6c8c88830061abe343a401644fc62
Harshit Batra <payafterwork.in@gmail.com>
2020-08-11 03:14:57 +0530

    Category update delete and get all

--- controllers/category.js
+++ controllers/category.js
@@ -29,3 +29,45 @@ exports.create = (req, res) => {
         res.json({ data });
     });
 };
+
+exports.update = (req, res) => {
+    console.log('req.body', req.body);
+    console.log('category update param', req.params.categoryId);
+
+    const category = req.category;
+    category.name = req.body.name;
+    category.save((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json(data);
+    });
+};
+
+
+exports.remove = (req, res) => {
+    const category = req.category;
+    category.remove((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            message: "Category deleted"
+        });
+    });
+};
+
+exports.list = (req, res) => {
+    Category.find().exec((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json(data);
+    });
+};
